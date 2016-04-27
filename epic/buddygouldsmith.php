fcf59fb7556d0c3df94505b7909143b22e294e17
Jared Padilla <jaredpadilla16@gmail.com>
2016-04-27 15:02:09 -0600

    Added buddygouldsmith persona and case use.

---
+++ epic/buddygouldsmith.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="ufc-8">
+		<title>Recruiting Coach</title>
+	</head>
+	<body>
+
+		<main>
+			Persona
+
+			Name:Buddy Gouldsmith
+
+			Age: 47 years old
+
+			Profession:Assistant Coach/Recruiting Coordinator
+
+			Technology: Mac User. Uses a 15" Macbook pro laptop and has an iPhone 6 plus with unlimited 4GLTE.
+
+			Attitudes and Behaviors: Has a fairly busy schedule, especially come baseball season. Although he has a busy schedule he scouts players thorughly. Uses social media to stay up to date on all baseball news.
+
+			Frustrations and Needs: Buddy needs to utilize his time as much as possible. He needs to know when and where certain players are playing. He is looking for players with these certain attributes that play this position.
+			Goals: Buddy Gouldsmith is looking for specific type of player to fill his team.
+
+			Use Case: Buddy Gouldsmith needs to find players with (x) certain attributes.
+			0) Buddy opens up chrome on his macbook.
+			1) Chrome loads up with a cursor blinking in his address bar.
+			2) In the address bar he types in (our website url) and hits enter.
+			3) The site loads up and buddy proceedes to login to the web app.
+			4) Buddy searches for players that meet what he's looking for (Position, Right/Left Handed)
+			5) He selects the players that meet what he needs.
+			6) From their he finds out when and where the players will be playing.
+		</main>
+	</body>
+</html>
