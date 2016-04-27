82d401760d8c5f54d520e84d0f0cd05eafa66b04
Francisco Garcia <ciscog11@msn.com>
2016-04-27 14:48:04 -0600

    added persona chris cook

---
+++ epic/chriscook.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<head>
+	<meta charset="UTF-8">
+	<meta lang="en">
+</head>
+<body>
+	<h2>Persona</h2>
+	<p>Chris Cook is an assistant coach for a Division II College baseball team. In the off-season part of his job is to recruit high school players to come be a student athlete at his College. This entails long days on the road in addition to trying to find out which players he needs to scout to be able to then recruit. Part of this is scouring the internet to find schedules and locations of games. He wants a place where he can find all of these things in one place</p>
+	<h2>Use Case</h2>
+	<p>Chris has just finished watching a weekday college game in Eugene Oregon. He's staying in a hotel
+		and pulls out his laptop. He goes to the web app and searches for a game that is within
+		driving distance of his hotel for tomorrow. He clicks on the game and then clicks on
+		each team to see about any players he may be interested in scouting Based on that
+		information he decides which game he will be going to but it's farther away then he was planning. The next day he pulls up the game on his phone and clicks on the
+		game location which then opens up google maps to give him directions to the game
+		as well as traffic info to get there on time.
+	</p>
+</body>
