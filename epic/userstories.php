1c7fdebfd8859e3ff09fef90b517975398632772
Francisco Garcia <ciscog11@msn.com>
2016-04-27 14:31:57 -0600

    added user stories and system goals

---
+++ epic/userstories.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h1>User Stories</h1>
+	<ul>
+		<li>As a team scout I want to find where a player is playing.</li>
+		<li>As a team scout I want to find the player tangibles</li>
+		<li>As a hs coach I want to post the tangible information about my players.</li>
+		<li>As a college coach I want to post about my players as well as find out hs recruits tangibles.</li>
+	</ul>
+</body>
