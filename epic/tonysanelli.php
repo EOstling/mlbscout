82b4319d94c48cf3525754b6f4e24956f2f007ba
Lucas Lauick <llaudick@cnm.edu>
2016-04-27 14:51:16 -0600

    added persona and use case for tony sanelli

---
+++ epic/tonysanelli.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h1>Tony Sanelli</h1>
+		<h2>Persona</h2>
+		<h3>Name: Tony Sanelli</h3>
+		<h3>Age: 45</h3>
+		<h3>Profession:</h3>
+			<p>Tony is the college coach for ...... his team practices everyday and has games every weekend. With tony's
+			busy schedule he only has so much time to show off his players abilities so they can be scouted by some Major League
+			baseball teams.</p>
+
+		<h3>Technology:</h3>
+			<p>Mac Pro Desktop, Mac laptop at home or used for the office, or Iphone 6 with mobile app availability.</p>
+
+		<h3>Attitudes and Behaviors:</h3>
+			<p>long hours throughout the week with practices highschool scouting and trying to get his current
+			players out on the market. With one of the main goals for tony having a successful college season there are priorities when
+			it comes to scouting, promoting, and playing. Tony needs a easy way to be able to promote players fast and efficiently.</p>
+
+		<h3>Frustrations and Needs:</h3>
+			<p>With the long hours that come with being the head coach of the college baseball team, time and
+			organization is key to success. Promoting players can be hard when there isn't much time and can be difficult to properly
+			promote the players on the team. Tony needs a way to be able promote his players nationwide with the time that is given to
+			him.</p>
+
+		<h3>Goals:</h3>
+			<p>Tony's main goals are to promote his players at a level that will attract the mlb scouts.</p>
+
+		<h2>Use Case</h2>
+		<h3>Goal: To post statistics about players</h3>
+		<p>the college coach will access the MLB scout website and let it load
+			press the login button and enter his information
+			the website will either accept or decline the information and proceed to load the coaches profile if it is accepted
+			then will press on a link to load the players on the team
+			once this loads the coach can now input the data on the players they choose</p>
+	</body>
+</html>
