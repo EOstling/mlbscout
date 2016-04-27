7e66e2084adc842f1dc4da57a9a299eac62796d2
Eliot Ostling <it.treugott@gmail.com>
2016-04-27 14:53:52 -0600

    Editing Executive Summary

--- epic/executivesummary.php
+++ epic/executivesummary.php
@@ -13,26 +13,5 @@
 	be more effective getting to more players in less time. Validated High School
 	coaches will be able to submit information like rosters and schedules in order to
 	promote their players and team. Players will benefit
-	1) Team Scout: Uses the web app to find general information on a player as well as
-	finding where said player has a game and if player is playing.
-	HS coach: Uses the web app to get his players noticed who wouldn't normally be while growing
-	his team be it school or travel team.
-	College Coach: Uses the web app to find general information on a player as well as
-	finding where said player has a game and for recruiting purposes. Also get's players
-	noticed that might not be(especially for DIII and DII coaches)
-	Players: The web app provides them with more publicity and noteriety that they
-	would not be getting other wise.
-	2) Team Scout: As a team scout I want to find where a player is playing.
-	As a team scout I want to find the player tangibles
-	Hs Coach: As a hs coach I want to post the tangible information about my players.
-	College Coach: As a college coach I want to post about my players as well as
-	find out hs recruits tangibles.3) Team Scout Use Case:
-	Joe has just finished watching a weekday college game in Eugene Oregon. He's staying in a hotel
-	and pulls out his laptop. He goes to the web app and searches for a game that is within
-	driving distance of his hotel for tomorrow. He clicks on the game and then clicks on
-	each team to see about any players he may be interested in scouting Based on that
-	information he decides which game he will be going to but it's farther away then he
-	was planning. The next day he pulls up the game on his phone and clicks on the
-	game location which then opens up google maps to give him directions to the game
-	as well as traffic info to get there on time.
+
 </body>
