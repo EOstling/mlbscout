b8df2b808b60dd1cb2e4caa1f5cdbca436df5f5e
Jared Padilla <jaredpadilla16@gmail.com>
2016-04-28 15:38:17 -0600

    Added heading elements.

--- epic/buddygouldsmith.php
+++ epic/buddygouldsmith.php
@@ -7,22 +7,22 @@
 	<body>
 
 		<main>
-			Persona
+			<h2>Persona</h2>
 
-			Name:Buddy Gouldsmith
+			<h3>Name:</h3>Buddy Gouldsmith
 
-			Age: 47 years old
+			<h3>Age:</h3> 47 years old
 
-			Profession:Assistant Coach/Recruiting Coordinator
+			<h3>Profession:</h3>Assistant Coach/Recruiting Coordinator
 
-			Technology: Mac User. Uses a 15" Macbook pro laptop and has an iPhone 6 plus with unlimited 4GLTE.
+			<h3>Technology:</h3> Mac User. Uses a 15" Macbook pro laptop and has an iPhone 6 plus with unlimited 4GLTE.
 
-			Attitudes and Behaviors: Has a fairly busy schedule, especially come baseball season with all his practives and games. Although he has a busy schedule he scouts players thoroughly. Uses social media to stay up to date on all baseball news.
+			<h3>Attitudes and Behaviors:</h3> Has a fairly busy schedule, especially come baseball season with all his practives and games. Although he has a busy schedule he scouts players thoroughly. Uses social media to stay up to date on all baseball news.
 
-			Frustrations and Needs: Buddy needs to utilize his time as much as possible. He needs to know when and where certain players are playing. He is looking for players with these certain attributes that play this position.
+			<h3>Frustrations and Needs:</h3> Buddy needs to utilize his time as much as possible. He needs to know when and where certain players are playing. He is looking for players with these certain attributes that play this position.
 			Goals: Buddy Gouldsmith is looking for a player that plays (x) position and throws with his (h) hand.
 
-			Use Case: Buddy Gouldsmith needs to find players who play (x) position and are (h) handed.
+			<h2>Use Case:</h2> Buddy Gouldsmith needs to find players who play (x) position and are (h) handed.
 			0) Buddy opens up chrome on his macbook.
 			1) Chrome loads up with a cursor blinking in his address bar.
 			2) In the address bar he types in (our website url) and hits enter.
