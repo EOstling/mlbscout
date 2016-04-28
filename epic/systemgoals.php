f0ad98333f9a547b277d4a274fd66130c6517a8f
Lucas Lauick <llaudick@cnm.edu>
2016-04-28 16:25:23 -0600

    edited systemgoals file

--- epic/systemgoals.php
+++ epic/systemgoals.php
@@ -1,15 +1,15 @@
 <!DOCTYPE HTML>
-
-<head>
-
-</head>
-<h1>System Goals</h1>
-<body>
-	<p>
-		<ol>
-			<li>Team Scout: Uses the web app to find general information on a player as well as finding where said player has a game and if player is playing.</li>
-			<li>HS coach: Uses the web app to get his players noticed who wouldn't normally be while growing his team be it school or travel team.</li>
-			<li>College Coach: Uses the web app to find general information on a player as well as finding where said player has a game and for recruiting purposes. Also get's players noticed that might not be(especially for DIII and DII coaches)</li>
-			<li>Players: The web app provides them with more publicity and noteriety that they would not be getting other wise.</li>
-	   </ol>
-</body>
+<html lang="em">
+	<head>
+		<meta charset="UTF-8">
+	</head>
+	<h1>System Goals</h1>
+	<body>
+			<ol>
+				<li>Team Scout: Uses the web app to find general information on a player as well as finding where said player has a game and if player is playing.</li>
+				<li>HS coach: Uses the web app to get his players noticed who wouldn't normally be while growing his team be it school or travel team.</li>
+				<li>College Coach: Uses the web app to find general information on a player as well as finding where said player has a game and for recruiting purposes. Also get's players noticed that might not be(especially for DIII and DII coaches)</li>
+				<li>Players: The web app provides them with more publicity and noteriety that they would not be getting other wise.</li>
+	   	</ol>
+	</body>
+</html>
