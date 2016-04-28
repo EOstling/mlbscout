bba51b839e85e3523dfd98b19f59d22005a01ea4
Eliot Ostling <it.treugott@gmail.com>
2016-04-28 15:34:39 -0600

    updating mike michael

--- epic/mikemichael.php
+++ epic/mikemichael.php
@@ -1,29 +1,36 @@
 <!DOCTYPE html>
-<head>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" href="../css/style.css" type="text/css"/>
-</head>
-<!--@see http://msboolean.com/hipster-baby-name-generator/ -->
-<header>BASIL MAYNARD DEVON  USE-CASE/Persona</header>
+<html>
+	<head>
+		<meta charset="utf-8">
+	</head>
 	<body>
-		<p>
+		<h1>Mike Michael</h1>
+		<h2>Persona</h2>
+		<h3>Name: Mike Michael</h3>
+		<h3>Age: 32</h3>
+		<h3>Profession: High School Coach</h3>
+		<p>Mike is the High School coach for his school baseball's team. On top of coaching as a side hobby Mike also
+		engages in sports statistics and loves researching baseball stats to include his own players stats.</p>
 
-			PERSONA:
-			Name: Basil Maynard Devon
-			Age: 25
+		<h3>Technology:</h3>
+		<p> 15' Dell Latp Intel Corei5. </p>
 
-			Software Developer : Basil, is a software developer who for fun makes websites pro-bono has developed many
-			different websites that use external API's and loves integrting whenever possible.
+		<h3>Attitudes and Behaviors:</h3>
+		<p> Mid to long hours depending on the time of the year. On the field he is very detail orineted and is very good
+			on in game analysis to provide motivation for his players regardless of the actual score on the field. </p>
 
+		<h3>Frustrations and Needs:</h3>
+		<p> Mike's main frustration is his lack of marketing he can do for his players. As well as he finds it extremely
+			 difficult to get reliable data for his own scouting purposes and if he wasn't in contact with other coaches
+			 and word of mouth he wouldn't have much avenues of approach at all.</p>
 
-			USE-CASE:
-			Basil Maynard Devon: After a scrum meeting goes back to his seat and does a quick duckduckgo.com search
-			on most useful
-		</p>
+		<h3>Goals:</h3>
+		<p>Mike's main goals are to get as much coverage for his own players to advance to Division 1 school. </p>
 
-
-
-
-</body>
+		<h2>Use Case</h2>
+		<h3>Goal: To upload as much data on his players as possible to attact other scouts that maybe in the area.</h3>
+		<p>The High coach will access the scouting app and be able to navigate to upload his players page. He will able
+		to input measurements , position, height, weight. He will be putting his full roster in hopes that College scouts
+		will take his players into consideration</p>
+	</body>
 </html>
-
