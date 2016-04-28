e49ff2bfb4ee6497373c03ec48e9ebcc962885b9
Francisco Garcia <ciscog11@msn.com>
2016-04-28 16:04:02 -0600

    added to chriscook persona and use case

--- epic/chriscook.php
+++ epic/chriscook.php
@@ -4,15 +4,21 @@
 	<meta lang="en">
 </head>
 <body>
-	<h2>Persona</h2>
-	<p>Chris Cook is an assistant coach for a Division II College baseball team. In the off-season part of his job is to recruit high school players to come be a student athlete at his College. This entails long days on the road in addition to trying to find out which players he needs to scout to be able to then recruit. Part of this is scouring the internet to find schedules and locations of games. He wants a place where he can find all of these things in one place</p>
-	<h2>Use Case</h2>
-	<p>Chris has just finished watching a weekday college game in Eugene Oregon. He's staying in a hotel
-		and pulls out his laptop. He goes to the web app and searches for a game that is within
-		driving distance of his hotel for tomorrow. He clicks on the game and then clicks on
-		each team to see about any players he may be interested in scouting Based on that
-		information he decides which game he will be going to but it's farther away then he was planning. The next day he pulls up the game on his phone and clicks on the
-		game location which then opens up google maps to give him directions to the game
-		as well as traffic info to get there on time.
-	</p>
+	<h1>Persona</h1>
+	<h2>Chris Cook</h2>
+	<h2>Age: 32</h2>
+	<h2>Profession:</h2>
+		<p>Chris Cook is a professional baseball scout for a Major League Baseball Team. His job is to find talent in his assigned region in order for his team to draft and sign the talent to a professional contract. The talent consists of all High School and College baseball players which he must then narrow down to around 20 players that his Team will then attempt to draft or not draft on his recommendation.</p>
+	<h2>Technology:</h2>
+		<p>Since Chris is always on the road traveling for his prospects' games his iPhone 6+ is his largest tool. He also has a laptop that he brings and uses when he's back home or in a hotel room after a road trip </p>
+	<h2>Attitudes and Behaviors</h2>
+		<p>Chris never has a day off. During the weekdays traveling to local high school and college games based around the city he lives in. During the weekends traveling to out of state games in his region which are primarily college 3 game series .</p>
+	<h2>Frustrations and Needs:</h2>
+		<p>Chris' main concern is that he needs to know when his favorite players are playing as well as potential players he will be interested in. He can't afford to miss an opportunity to see a player he is interested in because the more times he sees a player the better idea of how good the player is becomes. Being organized and having a quick reference to when and where players are playing is essential to seeing as many players as possible. He has to travel hundreds of miles to see players and being organized allows him to see as many players as possible.
+		</p>
+	<h2>Goal</h2>
+		<p>Chris's main goal is to find the top talent from the biggest city to the smallest town in order to give his team the best chance to draft the best players.</p>
+	<h1>Use Case</h1>
+	<h2>Goal</h2>
+		<p>Chris will unlock his phone and open up the mobile web browser. He will direct the browser to the web app. Then he will click the search bar and enter the name of a player he needs to see later in the day. He clicks search and the app gives him the result he wants. He navigates to the players page and sees on the bottom of the page the schedule listed and the info he needs. </p>
 </body>
