ffcabe2878f45f0bf88382edc91757d9d37daa94
Eliot Ostling <it.treugott@gmail.com>
2016-04-28 15:50:41 -0600

    updating Basil

--- epic/basilMaynardDevon.php
+++ epic/basilMaynardDevon.php
@@ -1,25 +1,34 @@
 <!DOCTYPE html>
-<head>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" href="../css/style.css" type="text/css"/>
-</head>
-<!--@see http://msboolean.com/hipster-baby-name-generator/ -->
-<header>BASIL MAYNARD DEVON  USE-CASE/Persona</header>
-<body>
-	<p>
+<html>
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h1>Basil Maynard Devon</h1>
+		<h2>Persona</h2>
+		<h3>Name: Basil Maynard Devon</h3>
+		<h3>Age: 25</h3>
+		<h3>Profession: Software Developer</h3>
+		<p>Basil, is a software developer who does Website developement for a baseball company. As a
+			side hobby he does enjoy baseball to playing Go. </p>
 
-		PERSONA:
-		Name: Basil Maynard Devon
-		Age: 25
+		<h3>Technology:</h3>
+		<p> Macbook pro Desktop IMac  </p>
 
-		Software Developer : Basil, is a software developer who for fun makes websites has developed many
-		different websites that use external API's and loves integrting whenever possible.
+		<h3>Attitudes and Behaviors:</h3>
+		<p>Basil, as a software developer is very excited about new technologies and about Databases. He has himself
+			used API's and also build them for both his personal use and for his company</p>
 
+		<h3>Frustrations and Needs:</h3>
+		<p> From a developer perspective he doesn't have a real way of having accurate data from an API or even a good source
+			 that his baseball team can use for scouting purposes.</p>
 
-		USE-CASE:
-		Basil Maynard Devon: After a scrum meeting goes back to his seat and does a quick duckduckgo.com search
-		on most upincoming web API's and finds that there is none on the horizon. He does this because his hobby is
-		amatuer baseball and was thinking of writing his own app and popped up mlbscouter.
-	</p>
+		<h3>Goals:</h3>
+		<p>Basil, his main goal is to get accurate uncorputed data </p>
+
+		<h2>Use Case</h2>
+		<h3>With developer access, he is able to view/import data from many different teams that he finds intriguing. </h3>
+		<p>He can navigate easily from player profiles to the actual data set and be able to start a session and begin
+			using the data from an API for analytical purposes.</p>
 	</body>
-	</html>
+</html>
