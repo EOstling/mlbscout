aa028f1ebdc42531e82a6951cd044826853d4640
Jared Padilla <jaredpadilla16@gmail.com>
2016-04-28 15:18:40 -0600

    Merge branch 'master' of /Users/JPadilla16/Desktop/BootCamp/Git/mlbscout with conflicts.

--- epic/executivesummary.php
+++ epic/executivesummary.php
@@ -4,14 +4,14 @@
 
 </head>
 <body>
-	<p>
-	Scouting web app that fulfills the hole left by the MLB bureu scout service.
+
+
+
+	0) Scouting web app that fulfills the hole left by the MLB bureau scout service.
 	The core idea being that the web app provides general information on player
 	tangibles in addition to where and when their team is playing. This helps the scout
 	be more effective getting to more players in less time. Validated High School
 	coaches will be able to submit information like rosters and schedules in order to
 	promote their players and team. Players will benefit
-	</p>
 
 </body>
-</html>
