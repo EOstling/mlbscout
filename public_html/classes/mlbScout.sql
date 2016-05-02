645731d6336fc914cf8d7ca53b28957cc5c247f8
Eliot Ostling <it.treugott@gmail.com>
2016-05-01 18:01:47 -0600

    Updating Mysql file https://app.asana.com/0/117435671931045/118830311351363

--- public_html/classes/mlbScout.sql
+++ public_html/classes/mlbScout.sql
@@ -7,5 +7,51 @@ DROP TABLE IF EXISTS accessLevel;
 DROP TABLE IF EXISTS schedule;
 
 CREATE TABLE user(
+	userId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	userAccessLevel INT UNSIGNED NOT NULL,
+	userApiCall VARCHAR (32),
+	userActivationToken INT,
+	userEmail VARCHAR(64) NOT NULL,
+	userFirstName VARCHAR(32) NOT NULL,
+	userHash INT,
+	userLastName VARCHAR(32) NOT NULL,
+	userPassword VARCHAR(64) NOT NULL,
+	userPhoneNumber INT NOT NULL,
+	userSalt INT,
+	userUpdate VARCHAR UNSIGNED,
+	UNIQUE (EMAIL),
+	INDEX(userId),
+	FOREIGN KEY (userApiCall) REFERENCES ApiCall(userApiCall),
+	FOREIGN KEY (userAccessLevel) REFERENCES AccessLevel(userAccessLevel)
+	PRIMARY KEY(userId),
+);
 
+CREATE TABLE player (
+ playerId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+ playerUserId INT UNSIGNED NOT NULL,
+ playerBatting INT,
+ playerCommitment VARCHAR(128)
+ playerFirstName VARCHAR (32)
+ playerHealthStatus VARCHAR(64)
+ playerHeight INT
+ playerHomeTown VARCHAR(64)
+ playerLastName VARCHAR (32)
+ playerPosition VARCHAR (32)
+ playerThrowingHand VARCHAR(32)
+ playerUpdate VARCHAR (32)
+ INDEX(playerUserId),
+ FOREIGN KEY(playerUserId) REFERENCES playerUser(playerUserId),
+ PRIMARY KEY(playerId)
+);
+
+CREATE TABLE schedule (
+ scheduleId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+ scheduleTeamId INT UNSIGNED NOT NULL,
+ scheduleDate DATETIME NOT NULL
+ schedulelocation VARCHAR(64)
+ scheduleStartingPosition VARCHAR(32)
+ scheduleTime VARCHAR(32)
+ INDEX(scheduleTeamId),
+ FOREIGN KEY(scheduleTeamId) REFERENCES scheduleTeam(scheduleTeamId),
+ PRIMARY KEY(scheduleId)
 );
