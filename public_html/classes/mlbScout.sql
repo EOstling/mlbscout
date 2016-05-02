846b2e125c26381650ec28f1cf65db8e176cd238
Eliot Ostling <it.treugott@gmail.com>
2016-05-02 13:37:41 -0600

    Fixing syntax https://app.asana.com/0/117435671931045/118830311351363

--- public_html/classes/mlbScout.sql
+++ public_html/classes/mlbScout.sql
@@ -9,7 +9,7 @@ DROP TABLE IF EXISTS schedule;
 CREATE TABLE user(
 	userId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	userAccessLevel INT UNSIGNED NOT NULL,
-	userApiCall VARCHAR (32),
+	userApiCall VARCHAR (2000),
 	userActivationToken INT,
 	userEmail VARCHAR(64) NOT NULL,
 	userFirstName VARCHAR(32) NOT NULL,
@@ -18,12 +18,12 @@ CREATE TABLE user(
 	userPassword VARCHAR(64) NOT NULL,
 	userPhoneNumber INT NOT NULL,
 	userSalt INT,
-	userUpdate VARCHAR UNSIGNED,
+	userUpdate VARCHAR(64),
+	INDEX (userId),
 	UNIQUE (userEmail),
-	INDEX(userId),
 	FOREIGN KEY (userApiCall) REFERENCES ApiCall(userApiCall),
-	FOREIGN KEY (userAccessLevel) REFERENCES AccessLevel(userAccessLevel)
-	PRIMARY KEY(userId)
+	FOREIGN KEY (userAccessLevel) REFERENCES accessLevel(userAccessLevel),
+	PRIMARY KEY (userId)
 );
 
 CREATE TABLE player (
@@ -36,29 +36,28 @@ CREATE TABLE player (
  playerHeight INT,
  playerHomeTown VARCHAR(64),
  playerLastName VARCHAR (32),
- playerPosition VARCHAR (32),
- playerThrowingHand VARCHAR(16),
+ playerPosition VARCHAR (8),
+ playerThrowingHand VARCHAR(2),
  playerUpdate VARCHAR (32),
  INDEX(playerUserId),
- FOREIGN KEY(playerUserId) REFERENCES playerUser(playerUserId),
+ FOREIGN KEY(playerUserId) REFERENCES player(playerUserId),
  PRIMARY KEY(playerId)
 );
 
 CREATE TABLE schedule (
  scheduleId INT  AUTO_INCREMENT NOT NULL,
- scheduleTeamId INT UNSIGNED NOT NULL,
- scheduleDate DATETIME NOT NULL
- schedulelocation VARCHAR(64)
- scheduleStartingPosition VARCHAR(32)
- scheduleTime VARCHAR(32)
+ scheduleTeamId INT  ,
+ scheduleLocation VARCHAR(64),
+ scheduleStartingPosition VARCHAR(32),
+ scheduleTime DATETIME,
  INDEX(scheduleTeamId),
- FOREIGN KEY(scheduleTeamId) REFERENCES scheduleTeam(scheduleTeamId),
+ FOREIGN KEY(scheduleTeamId) REFERENCES schedule(scheduleTeamId),
  PRIMARY KEY(scheduleId)
 );
 
-CREATE TABLE apiCall(
+CREATE TABLE ApiCall(
 apiCallId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-apiCallUserId UNSIGNED NOT NULL,
+apiCallUserId  INT,
 apiCallDatetime DATETIME NOT NULL,
 apiCallQueryString VARCHAR(2000),
 apiCallURL VARCHAR(32),
@@ -75,7 +74,7 @@ CREATE TABLE team(
 teamId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 teamType VARCHAR(32) NOT NULL,
 teamName VARCHAR (64) NOT NULL,
-teamStarters VARCHAR(64)
+teamStarters VARCHAR(64),
 INDEX(teamId),
 UNIQUE (teamId),
 PRIMARY KEY (teamId)
@@ -99,6 +98,6 @@ FOREIGN KEY(favoriteUserId)REFERENCES favorite(favoriteUserId)
 CREATE TABLE favoritePlayer(
 favoritePlayerId INT UNSIGNED NOT NULL,
 favoriteUserId INT NOT NULL,
-INDEX(favoriteUserId)
+INDEX(favoriteUserId),
 PRIMARY KEY (favoritePlayerId)
 );
