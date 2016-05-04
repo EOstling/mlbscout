7a4f1a1cb257d9e09633dd950d42d9389a6787c4
Eliot Ostling <it.treugott@gmail.com>
2016-05-04 15:45:25 -0600

    adding closure & other testing https://app.asana.com/0/117435671931045/118830311351373

--- public_html/test/ApiCallTest.php
+++ public_html/test/ApiCallTest.php
@@ -6,11 +6,11 @@
  * @author Eliot Robert Ostling <it.treugott@gmail.com>
  **/
 
-require_once("ApiCallTest.php");
+require_once("MlbScoutTest.php");
 
-//require_once(apiCall(__DIR__) . "/mlbscout/php/classes/apiCall/autoload.php");
+//require_once(apiClass(__DIR__) . "/mlbscout/php/classes/apiCall/autoload.php");
 
-class apiClass extends ApiCallTest {
+class apiClass extends MlbScoutTest {
 
 	/**
 	 * Timestamp of the API Date time; this starts as null and is assigned later
@@ -23,16 +23,67 @@ class apiClass extends ApiCallTest {
 	 **/
 	protected $VALID_APIQUERYCONTENT = "PHPUnit test passing";
 	/**
-	 * Content for URL
+	 * Valid URL
 	 * @var string $VALID_APIURL
 	 */
 	protected $VALID_APIURL;
-
 	/**
-	 *
-	 *
+	 * Valid HttpVerb
+	 * @var null
 	 */
+	protected $VALID_APIHTTPVERB=null;
+	/**
+	 * Valid Browser
+	 * @var
+	 */
+	protected $VALID_APIBROWSER;
+	/**
+	 * Checking for valid IP
+	 * @var
+	 */
+	protected $VALID_APIIP;
+	/**
+	 * Checking for payload
+	 * @var
+	 */
+	protected $VALID_APIPAYLOAD;
+	/**
+	 * @var USER null
+	 */
+	protected $VALID_ApiUserId=null;
 
+
+	//Creating the setup objects before testing
+
+	public final function setUp(){
+		//setup method first
+		parent::setUp();
+		//Create state variables of string & integer
+		$this->ApiUser = new ApiUser(null, "@phpunit", "test@phpunit.de", "+12125551212");
+		$this->ApiUser->insert($this->getPDO());
+		//Timestamp of Test being ran
+		$this->VALID_APIDATETIME = new \DateTime();
+	}
+
+	public function testInsertValidAPIDATETIME(){
+		/**
+		 count the number of rows and save it for later
+		**/
+
+		$numRows = $this->getConnection()->getRowCount("tweet");
+
+		// create a Datetime and insert it into mySQL
+		$apiCall = new apiCall (null,$this->VALID_APIDATETIME, $this->APIQUERYCONTENT,
+										$this->APIURL,$this->APIHTTPVERB, $this->APIBROWSER,
+										$this->APIIP,$this->APIPAYLOAD,$this->ApiUserId);
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoApiCall = apicall::getDateTimebyUserId($this->getPDO(), $tweet->getTweetId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("tweet"));
+		$this->assertEquals($pdoTweet->getProfileId(), $this->profile->getProfileId());
+		$this->assertEquals($pdoTweet->getTweetContent(), $this->VALID_TWEETCONTENT);
+		$this->assertEquals($pdoTweet->getTweetDate(), $this->VALID_TWEETDATE);
+	}
 
 
 
