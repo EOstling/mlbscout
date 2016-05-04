3eaa8a95606c89ff1c30839f2e3ec71b2b65b049
Eliot Ostling <it.treugott@gmail.com>
2016-05-04 11:18:00 -0600

    Updating everything https://app.asana.com/0/117435671931045/118830311351363

--- mlbScout.sql
+++ mlbScout.sql
@@ -2,65 +2,64 @@ DROP TABLE IF EXISTS apiCall;
 DROP TABLE IF EXISTS accessLevel;
 DROP TABLE IF EXISTS favoritePlayer;
 DROP TABLE IF EXISTS schedule;
-DROP TABLE IF EXISTS team;
 DROP TABLE IF EXISTS player;
+DROP TABLE IF EXISTS team;
 DROP TABLE IF EXISTS user;
 
 CREATE TABLE user(
 	userId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-	useraccessLevel INT UNSIGNED NOT NULL,
-	userapiCall VARCHAR (2000),
-	userActivationToken INT,
+	userAccessLevel INT UNSIGNED NOT NULL,
+	userActivationToken CHAR(32),
 	userEmail VARCHAR(64) NOT NULL,
 	userFirstName VARCHAR(32) NOT NULL,
-	userHash INT,
+	userHash CHAR(128) NOT NULL,
 	userLastName VARCHAR(32) NOT NULL,
 	userPassword VARCHAR(64) NOT NULL,
 	userPhoneNumber INT NOT NULL,
-	userSalt INT,
+	userSalt CHAR(64) NOT NULL,
 	userUpdate VARCHAR(64),
 	INDEX(userId),
 	UNIQUE(userEmail),
 	PRIMARY KEY (userId)
 );
 
+CREATE TABLE team(
+	teamId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	teamType VARCHAR(32) NOT NULL,
+	teamName VARCHAR(64) NOT NULL,
+	INDEX(teamId),
+	PRIMARY KEY (teamId)
+);
+
 CREATE TABLE player (
 	playerId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	playerTeamId INT UNSIGNED NOT NULL,
 	playerUserId INT UNSIGNED NOT NULL,
-	playerBatting INT,
+	playerBatting CHAR(1) NOT NULL,
 	playerCommitment VARCHAR(128),
-	playerFirstName VARCHAR (32),
+	playerFirstName VARCHAR(32) NOT NULL,
 	playerHealthStatus VARCHAR(64),
-	playerHeight INT,
-	playerHomeTown VARCHAR(64),
-	playerLastName VARCHAR (32),
-	playerPosition VARCHAR (8),
-	playerThrowingHand VARCHAR(2),
-	playerUpdate VARCHAR (32),
+	playerHeight TINYINT UNSIGNED NOT NULL,
+	playerHomeTown VARCHAR(64) NOT NULL,
+	playerLastName VARCHAR(32) NOT NULL,
+	playerPosition VARCHAR(8) NOT NULL,
+	playerThrowingHand CHAR(1) NOT NULL,
+	INDEX(playerTeamId),
 	INDEX(playerUserId),
 	INDEX(playerId),
 	FOREIGN KEY (playerUserId)REFERENCES user(userId),
 	PRIMARY KEY(playerId)
 );
 
-CREATE TABLE team(
-	teamId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-	teamType VARCHAR(32) NOT NULL,
-	teamName VARCHAR (64) NOT NULL,
-	teamRoster VARCHAR(64),
-	INDEX(teamId),
-	PRIMARY KEY (teamId)
-);
-
 CREATE TABLE schedule (
-	scheduleId INT  UNSIGNED AUTO_INCREMENT NOT NULL,
-	scheduleTeamId INT UNSIGNED NOT NULL  ,
-	scheduleLocation VARCHAR(64),
-	scheduleStartingPosition VARCHAR(32),
-	scheduleTime DATETIME,
+	scheduleId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	scheduleTeamId INT UNSIGNED NOT NULL,
+	scheduleLocation VARCHAR(64) NOT NULL,
+	scheduleStartingPosition VARCHAR(32) NOT NULL,
+	scheduleTime DATETIME NOT NULL,
 	INDEX(scheduleId),
 	INDEX(scheduleTeamId),
-	FOREIGN KEY(scheduleTeamId)REFERENCES team(TeamId),
+	FOREIGN KEY(scheduleTeamId)REFERENCES team(teamId),
 	PRIMARY KEY(scheduleId,scheduleTeamId)
 );
 
@@ -76,22 +75,22 @@ CREATE TABLE favoritePlayer(
 
 CREATE TABLE accessLevel(
 	accessLevelId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-	accessLevelName VARCHAR(2),
+	accessLevelName VARCHAR(24),
 	PRIMARY KEY(accessLevelId)
 );
 
 CREATE TABLE apiCall(
 	apiCallId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	apiCallUserId INT UNSIGNED NOT NULL,
-	apiCallDatetime DATETIME NOT NULL,
-	apiCallQueryString VARCHAR(2000),
-	apiCallURL VARCHAR(32),
-	apiCallHttpVerb VARCHAR (32),
-	apiCallBrowser VARCHAR(32),
-	apicallIP VARCHAR (32),
-	apiCallPayload VARCHAR(64),
-	INDEX(apiCallUserid),
+	apiCallDateTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	apiCallQueryString VARCHAR(128),
+	apiCallURL VARCHAR(128) NOT NULL,
+	apiCallHttpVerb VARCHAR(6) NOT NULL,
+	apiCallBrowser VARCHAR(128) NOT NULL,
+	apicallIP VARBINARY(16) NOT NULL,
+	apiCallPayload VARCHAR(2000),
+	INDEX(apiCallUserId),
 	INDEX(apiCallId),
-	FOREIGN KEY (apiCallUserId)REFERENCES user(UserId),
-	PRIMARY KEY (apiCallId,apiCallUserId)
+	FOREIGN KEY(apiCallUserId) REFERENCES user(UserId),
+	PRIMARY KEY(apiCallId, apiCallUserId)
 );
