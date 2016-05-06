c01f967e39b40c306b727f110717b15bfde670ad
Eliot Ostling <it.treugott@gmail.com>
2016-05-05 19:00:00 -0600

    Creating ApiClass-Included constructor https://app.asana.com/0/117435671931045/118830311351373

--- public_html/php/classes/ApiClass.php
+++ public_html/php/classes/ApiClass.php
@@ -1,7 +1,48 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ittre
- * Date: 5/4/2016
- * Time: 4:59 PM
- */
+class apiCall {
+
+	private $apiCallId;
+
+	private $apiCallDateTime;
+
+	private $callQueryString;
+
+	private $apiCallUserId;
+
+	private $apiCallUrl;
+
+	private $apiHttpVerb;
+
+	private $apiCallBrowser;
+
+	private $apiCallip;
+
+	private $apiCallPayload;
+
+	public function __construct($newApiCall, $newApiCallId, $newApiCallDateTime, $newApiCallQueryString,
+										 $newApiCallUserId, $newApiCallUrl, $newApiGET, $newApiPOST, $newApiPUT,
+										 $newApiDELETE, $newApiCallBrowser,
+										 $newApiCallip, $newApiCallPayload) {
+		try {
+			$this->setApiCall($newApiCall);
+			$this->setApicallId($newApiCallId);
+			$this->setApiCallDateTime($newApiCallDateTime);
+			$this->setApiCallQueryString($newApiCallQueryString);
+			$this->setApiCallUserId($newApiCallUserId);
+			$this->setApiCallUrl($newApiCallUrl);
+
+
+			$this->setApiGET($newApiGET);
+			$this->setApiPOST($newApiPOST);
+			$this->setApiPUT($newApiPUT);
+			$this->setApiDELETE($newApiDELETE);
+			$this->setApiCallBrowser($newApiCallBrowser);
+			$this->setApiCallip($newApiCallip);
+			$this->setApiCallPayload($newApiCallPayload);
+
+		} catch(UnexpectedValueException $exception) {
+			// rethrow to the USER
+			throw(new UnexpectedValueException("Unable to construct APICALL", 0, $exception));
+		}
+	}
+}
