d0a3a14a6a598a06f47746223bb750cede0fc13c
Eliot Ostling <it.treugott@gmail.com>
2016-05-09 10:17:57 -0600

    updating accessors & mutators

--- public_html/php/classes/ApiCall.php
+++ public_html/php/classes/ApiCall.php
@@ -1,57 +1,53 @@
 <?php
-class apiCall {
-	use ValidateDate;
-	/*
+class ApiCall {
+
+	/**
 	 * @var INT Primary key
-	 */
+	 **/
 	private $apiCallId;
-	/*
+	/**
 	 * @varINT DateTime
-	 */
+	 **/
 	private $apiCallDateTime;
-	/*
+	/**
 	 * @var INT Query
-	 */
+	 **/
 	private $callQueryString;
-	/*
+	/**
 	 * @var INT
-	 */
+	 **/
 	private $apiCallUserId;
-	/*
+	/**
 	 * @varINT
-	 */
-	private $apiCallUrl;
-	/*
+	 **/
+	private $apiCallURL;
+	/**
 	 * @var string
-	 */
+	 **/
 	private $apiHttpVerb;
-	/*
+	/**
 	 * @var string
-	 */
+	 **/
 	private $apiCallBrowser;
-	/*
+	/**
 	 * @var string
-	 */
+	 **/
 	private $apiCallip;
-	/*
-	 * @var VarBinary()
-	 */
+	/**
+	 * @var String()
+	 **/
 	private $apiCallPayload;
 
 	public function __construct( int $newApiCallId, TIMESTAMP $newApiCallDateTime, string $newApiCallQueryString,
-										int $newApiCallUserId,string  $newApiCallUrl, string $newApiGET,string $newApiPOST,string $newApiPUT,
-										string $newApiDELETE,string $newApiCallBrowser,
-										int $newApiCallip, string $newApiCallPayload) {
+										int $newApiCallUserId,string  $newApiCallURL, string $newApiCallHttpVerb,string $newApiCallBrowser,
+										string $newApiCallip, string $newApiCallPayload) {
 		try {
-			$this->setApicallId($newApiCallId);
+			$this->setapiCallId($newApiCallId);
 			$this->setApiCallDateTime($newApiCallDateTime);
 			$this->setApiCallQueryString($newApiCallQueryString);
 			$this->setApiCallUserId($newApiCallUserId);
-			$this->setApiCallUrl($newApiCallUrl);
-			$this->setApiGET($newApiGET);
-			$this->setApiPOST($newApiPOST);
-			$this->setApiPUT($newApiPUT);
-			$this->setApiDELETE($newApiDELETE);
+			$this->setApiCallURL($newApiCallURL);
+			$this->setApicallHttpVerb($newApiCallHttpVerb);
 			$this->setApiCallBrowser($newApiCallBrowser);
 			$this->setApiCallip($newApiCallip);
 			$this->setApiCallPayload($newApiCallPayload);
@@ -62,8 +58,8 @@ class apiCall {
 		}
 	}
 //ApiCallId
-	public function getApicallId(){
-	return($this->ApicallId);
+	public function getApiCallId(){
+	return($this->getapiCallId);
 }
 	public function setApicallId(int $newApiCallId){
 		$newApiCallId = filter_var($newApiCallId, FILTER_VALIDATE_INT);
@@ -72,8 +68,24 @@ class apiCall {
 		}
 
 		// convert and store the profile id
-		$this->ApicallId = intval($newApiCallId);
+		$this->apiCallId = intval($newApiCallId);
 	}
+
+	public function getApiCallUserId(){
+	return($this->apiCallUserId);
+	}
+
+	public function setApiCallUserId($newApiCallUserId){
+		$newApiCallUserId = filter_var($newApiCallUserId, FILTER_VALIDATE_INT);
+		if($newApiCallUserId === false) {
+			throw(new UnexpectedValueException("User id is not a valid integer"));
+		}
+
+		// convert and store the profile id
+		$this->apiCallUserId = intval($newApiCallUserId);
+
+	}
+
 //DateTime
 	public function getApiCallDateTime(){
 	return($this->apiCallDateTime);
@@ -91,9 +103,101 @@ class apiCall {
 		}
 
 	public function setApiCallQueryString($newApiCallQueryString){
+		$newApiCallQueryString = trim($newApiCallQueryString);
+		$newApiCallQueryString = filter_var($newApiCallQueryString, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+		if(empty($newApiCallQueryString) === true) {
+			throw(new \InvalidArgumentException(""));
+		}
+
+		// verify the tweet content will fit in the database
+
+		if(strlen($newApiCallQueryString) > 128) {
+			throw(new \RangeException("Range is too long"));
+		}
+
+		// store the tweet content
+
+		$this->getApiCallQueryString = $newApiCallQueryString;
+		}
+
+
+	public function getApiCallURL(){
+		return($this->apiCallURL);
+	}
+	public function setApiCallURL(string $newApiCallURL) {
+		$newApiCallURL = trim($newApiCallURL);
+		$newApiCallURL = filter_var($newApiCallURL, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+		if(empty($newApiCallURL) === TRUE) {
+			throw(new\InvalidArgumentException(""));
+
+			if(strlen($newApiCallURL) > 140) {
+				throw(new\RangeException("URL is too long"));
+			}
+			$this->getapiCallURL = $newApiCallUrl;
+		}
+	}
+		public function getApiCallHttpVerb(){
+			return($this->apiHttpVerb);
+		}
+
+		public function setApiCallHttpVerb(){
+
 
 		}
 
 
 
-}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+
+
