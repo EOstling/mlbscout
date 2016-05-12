01ae2453656e90cfbc453abc1b48b6c95e1bc8fc
Francisco Garcia <ciscog11@msn.com>
2016-05-11 23:24:22 -0600

    finishing up teamtest https://app.asana.com/0/117423711360920/118830311351371

--- public_html/test/TeamTest.php
+++ public_html/test/TeamTest.php
@@ -2,6 +2,7 @@
 namespace Edu\Cnm\MlbScout\TeamTest\Test;
 
 use Edu\Cnm\MlbScout\Team;
+use Edu\Cnm\MlbScout\Test\MlbScoutTest;
 
 // grab the project test parameters
 require_once("MlbScoutTest.php");
@@ -44,7 +45,7 @@ class TeamTest extends MlbScoutTest {
 	 */
 	public function testInsertValidTeam() {
 		// count the number of rows and save it for later
-		$numRows= $this->getConnection()-getRowCount("team");
+		$numRows= $this->getConnection()->getRowCount("team");
 
 		// create a new Team and insert into mySQL
 		$team = new Team(null, $this->VALID_TEAMNAME, $this->VALID_TEAMTYPE);
@@ -97,7 +98,7 @@ class TeamTest extends MlbScoutTest {
 	 */
 	public function testUpdateInvalidTeam() {
 		// create a Team with a non null team id and watch it fail
-		$team = new Team(null, $this->user-> $this->VALID_TEAMNAME, $this->VALID_TEAMTYPE);
+		$team = new Team(null, $this->VALID_TEAMNAME, $this->VALID_TEAMTYPE);
 		$team->update($this->getPDO());
 	}
 
@@ -147,6 +148,70 @@ class TeamTest extends MlbScoutTest {
 		// grab the data from mySQL
 		$pdoTeam = Team::getTeamByTeamId($this->getPDO(), $team->getTeamId());
 		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("team"));
+		$this->assertEquals($pdoTeam->getTeamName(), $this->VALID_TEAMNAME);
+		$this->assertEquals($pdoTeam->getTeamType(), $this->VALID_TEAMTYPE);
+	}
+
+	/**
+	 * test grabbing a Team that does not exist
+	 */
+	public function testGetInvalidTeamByTeamId() {
+		// grab a team id that exceeds the maximum allowable team id
+		$team = Team::getTeamByTeamId($this->getPDO(), MlbScoutTest::INVALID_KEY);
+		$this->assertNull($team);
+	}
+
+	/**
+	 * test grabbing a Team by team name
+	 */
+	public function testGetValidTeamByTeamName() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("team");
+
+		// create a new Team and insert to into mySQL
+		$team = new Team(null, $this->VALID_TEAMNAME, $this->VALID_TEAMTYPE);
+		$team->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$results = Team::getTeamByTeamName($this->getPDO(), $team->getTeamName());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("team"));
+		$this->assertCount(1, $results);
+		$this->assertContainsOnlyInstanceOf("Edu\\Cnm\\MlbScout\\Team", $results);
+
+		// grab the results from the array and validate it
+		$pdoTeam = $results[0];
+		$this->assertEquals($pdoTeam->getTeamName(), $this->VALID_TEAMNAME);
+		$this->assertEquals($pdoTeam->getTeamType(), $this->VALID_TEAMTYPE);
+	}
+
+	/**
+	 * test grabbing a Team by content that does not exist
+	 */
+	public function testGetInvalidTeamByTeamName() {
+		// grab a team by searching for name that does not exist
+		$team = Team::getTeamByTeamName($this->getPDO(), "you shall not pass");
+		$this->assertCount(0, $team);
+	}
+
+	/**
+	 * test grabbing all Teams
+	 */
+	public function testGetAllValidTeams() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("team");
+
+		// create a new Team and insert to into mySQL
+		$team = new Team(null, $this->VALID_TEAMNAME, $this->VALID_TEAMTYPE);
+		$team->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$results = Team::getAllTeams($this->getPDO());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("team"));
+		$this->assertCount(1, $results);
+		$this->assertContainsOnlyInstancesOf("Edu\\Cnm\\MlbScout\\Team", $results);
+
+		// grab the result from the array and validate it
+		$pdoTeam = $results[0];
 		$this->assertEquals($pdoTeam->getTeamName(), $this->VALID_TEAMNAME);
 		$this->assertEquals($pdoTeam->getTeamType(), $this->VALID_TEAMTYPE);
 	}
