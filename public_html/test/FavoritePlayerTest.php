45b1ddc9158b7e953d3626c7e6cdbd75312cd779
Francisco Garcia <ciscog11@msn.com>
2016-05-13 10:29:01 -0600

    added teamtest.php to phpunit.xml https://app.asana.com/0/117423711360920/118830311351371

---
+++ public_html/test/FavoritePlayerTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace Edu\Cnm\MlbScout\FavoritePlayerTest\Test;
+
+use Edu\Cnm\MlbScout\{Player, User};
+use Edu\Cnm\MlbScout\Test\MlbScoutTest;
+
+// grab the project test parameters
+require_once("MlbScoutTest.php");
+
+// grab the classes under scrutiny
+require_once(dirname(__DIR__) . "/php/classes/autoload.php");
+
+/**
+ * Full PHPUnit test for the favorite player class
+ *
+ * This is a complete PHPUnit test of the MlbScout favorite player class
+ * It is a complete test because ALL mySQL/PDO methods are tested for invalid and valid inputs
+ *
+ * @see FavoritePlayer
+ * @author Francisco Garcia <fgarcia132@cnm.edu
+ */
+class FavoritePlayerTest extends MlbScoutTest {
+	/**
+	 *
+	 */
+}
