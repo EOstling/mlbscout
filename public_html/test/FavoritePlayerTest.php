77443b4550ab5d835bfa32107254ef8258186c8d
Francisco Garcia <ciscog11@msn.com>
2016-05-16 10:24:36 -0600

    added to FavoritePlayerTest https://app.asana.com/0/117423711360920/118830311351377

--- public_html/test/FavoritePlayerTest.php
+++ public_html/test/FavoritePlayerTest.php
@@ -26,7 +26,88 @@ class FavoritePlayerTest extends MlbScoutTest {
 	 */
 	protected $VALID_PLAYERID = "PHPUnit test passing";
 	/**
-	 *
+	 * userId
+	 * @var string $VALID_USERID
 	 */
+	protected $VALID_USERID = "PHPUnit test passing";
+	/**
+	 * User that favorited the player; this is for foreign key relations
+	 */
+	protected $user = null;
+	/**
+	 * Player that was favorited; this is for foreign key relations
+	 */
+	protected $player = null;
+
+	/**
+	 * create dependent objects before running each test
+	 */
+	public final function setUp() {
+		// run the default setUp() method first
+		parent::setUp();
+
+		// create and insert a User to favorite a player
+		$this->user = new User(null, "@phpunit". "test@phpunit.de", "+12125551212");
+		$this->user->insert($this->getPDO());
+
+		// create and insert a Player that a user can favorite
+		$this->player = new Player(null, @"phpunit", "test@phpunit.de", "+12125551212");
+		$this->player->insert($this->getPDO());
+	}
+
+	/**
+	 * test inserting a valid User and verify that the mySQL data matches
+	 */
+	public function testInsertValidUser() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("user");
+
+		// create a new User and insert to into mySQL
+		$user = new User(null, $this->user->getUserId(), $this->VALID_USERID, $this->VALID_PLAYERID);
+		$user->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoUser = User::getUserByUserId($this->getPDO(), $user->getUserId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("user"));
+		$this->assertEquals($pdoUser->getUserId(), $this->user->getUserId());
+	}
+
+	/**
+	 * Test inserting a User that already exists
+	 * @expectedException PDOException
+	 */
+	public function testInsertInvalidUser() {
+		// create a User with a non null user id and watch it fail
+		$user = new User(MlbScoutTest::INVALID_KEY, $this->user->getUserId(), $this->player->getPlayerId());
+		$user->insert($this->getPDO());
+	}
+
+	/**
+	 * test inserting a valid Player and verify that the mySQL data matches
+	 */
+	public function testInsertValidPlayer() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("player");
+
+		// create a new Player and insert to into mySQL
+		$player = new Player(null, $this->player->getPlayerId(), $this->VALID_PLAYERID, $this->VALID_USERID);
+		$this->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoPlayer = Player::getPlayerByPlayerId($this->getPDO(), $player->getPlayerId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("player"));
+		$this->assertEquals($pdoPlayer->getPlayerid(), $this->player->getPlayerId());
+	}
+
+	/**
+	 * test inserting a Player that already exists
+	 * @expectedException \PDOException
+	 */
+	public function testInsertInvalidPlayer() {
+		// create a Player with a non null player id and watch it fail
+		$player = new Player(MlbScoutTest::INVALID_KEY, $this->player->getPlayerId(), $this->user->getUserId());
+	}
+
+
 
 }
