962d1575e8b8b51c5dc52fbd0158ba981d690ab9
Jared Padilla <jaredpadilla16@gmail.com>
2016-05-18 15:35:11 -0600

    updated lines 27 and 32 to equal "string"https://app.asana.com/0/117435671931068/118830311351365

--- public_html/php/classes/User.php
+++ public_html/php/classes/User.php
@@ -174,7 +174,7 @@ class User implements \JsonSerializable {
 	 * @throws \TypeError if $newUserActivationToken in not an integer
 	 */
 	public function setUserActivationToken(string $newUserActivationToken) {
-		if(!ctype_xdigit($newUserActivationToken) === false ) {
+		if(!ctype_xdigit($newUserActivationToken)) {
 			throw(new \InvalidArgumentException ("user activation is empty or insecure"));
 		}
 		if(strlen($newUserActivationToken) !== 32) {
