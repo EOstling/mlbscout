09f4153314ed1d93241f01269f2e90e5ae0c4f8e
Lucas Lauick <llaudick@cnm.edu>
2016-05-24 16:41:36 -0600

    edited player api

--- public_html/api/Player/index.php
+++ public_html/api/Player/index.php
@@ -62,7 +62,7 @@ try {
 		$requestContent = file_get_contents("php://input");
 		$requestObject = json_decode($requestContent);
 
-		//make sure tweet content is available
+		//make sure player batting is available
 		if(empty($requestObject->playerBatting) === true) {
 			throw(new \InvalidArgumentException ("No batting preference for player.", 405));
 		}
@@ -71,34 +71,401 @@ try {
 		//perform the actual put or post
 		if($method === "PUT") {
 
-			// retrieve the tweet to update
+			// retrieve the player to update
 			$player = MlbScout\Player::getPlayerByPlayerId($pdo, $id);
 			if($player === null) {
-				throw(new RuntimeException("Tweet does not exist", 404));
+				throw(new RuntimeException("player does not exist", 404));
 			}
 
-			// put the new tweet content into the tweet and update
-			$tweet->setPlayerBatting($requestObject->tweetContent);
-			$tweet->update($pdo);
+			// put the new player batting into the player and update
+			$player->setPlayerBatting($requestObject->playerBatting);
+			$player->update($pdo);
 
 			// update reply
 			$reply->message = "Player was updated OK";
-
 		} else if($method === "POST") {
 
-			//  make sure profileId is available
+			//  make sure userId is available
 			if(empty($requestObject->userId) === true) {
 				throw(new \InvalidArgumentException ("No User ID.", 405));
 			}
 
-			// create new tweet and insert into the database
+			// create new player and insert into the database
 			$player = new MlbScout\Player(null, $requestObject->userId, $requestObject->PlayerBatting, null);
 			$player->insert($pdo);
 
 			// update reply
 			$reply->message = "Player was created OK";
 		}
+	} else if($method === "PUT" || $method === "POST") {
 
+		verifyXsrf();
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
+
+		//make sure player commitment is available
+		if(empty($requestObject->playerCommitment) === true) {
+			throw(new \InvalidArgumentException ("No player commitment.", 405));
+		}
+
+
+		//perform the actual put or post
+		if($method === "PUT") {
+
+			// retrieve the player to update
+			$player = MlbScout\Player::getPlayerByPlayerId($pdo, $id);
+			if($player === null) {
+				throw(new RuntimeException("player does not exist", 404));
+			}
+
+			// put the new player batting into the player and update
+			$tweet->setPlayerCommitment($requestObject->playerCommitment);
+			$tweet->update($pdo);
+
+			// update reply
+			$reply->message = "Player was updated OK";
+		} else if($method === "POST") {
+
+			//  make sure userId is available
+			if(empty($requestObject->userId) === true) {
+				throw(new \InvalidArgumentException ("No User ID.", 405));
+			}
+
+			// create new player and insert into the database
+			$player = new MlbScout\Player(null, $requestObject->userId, $requestObject->PlayerCommitment, null);
+			$player->insert($pdo);
+
+			// update reply
+			$reply->message = "Player was created OK";
+		}
+	} else if($method === "PUT" || $method === "POST") {
+
+		verifyXsrf();
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
+
+		//make sure player first name is available
+		if(empty($requestObject->playerFirstName) === true) {
+			throw(new \InvalidArgumentException ("No player First Name.", 405));
+		}
+
+
+		//perform the actual put or post
+		if($method === "PUT") {
+
+			// retrieve the player to update
+			$player = MlbScout\Player::getPlayerByPlayerId($pdo, $id);
+			if($player === null) {
+				throw(new RuntimeException("player does not exist", 404));
+			}
+
+			// put the new player first name into the player and update
+			$tweet->setPlayerFirstName($requestObject->playerFirstName);
+			$tweet->update($pdo);
+
+			// update reply
+			$reply->message = "Player was updated OK";
+		} else if($method === "POST") {
+
+			//  make sure userId is available
+			if(empty($requestObject->userId) === true) {
+				throw(new \InvalidArgumentException ("No User ID.", 405));
+			}
+
+			// create new player and insert into the database
+			$player = new MlbScout\Player(null, $requestObject->userId, $requestObject->PlayerFirstName, null);
+			$player->insert($pdo);
+
+			// update reply
+			$reply->message = "Player was created OK";
+		}
+	} else if($method === "PUT" || $method === "POST") {
+
+		verifyXsrf();
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
+
+		//make sure player health status is available
+		if(empty($requestObject->playerHealthStatus) === true) {
+			throw(new \InvalidArgumentException ("No player Health Status.", 405));
+		}
+
+
+		//perform the actual put or post
+		if($method === "PUT") {
+
+			// retrieve the player to update
+			$player = MlbScout\Player::getPlayerByPlayerId($pdo, $id);
+			if($player === null) {
+				throw(new RuntimeException("player does not exist", 404));
+			}
+
+			// put the new player health status into the player and update
+			$tweet->setPlayerHealthStatus($requestObject->playerHealthStatus);
+			$tweet->update($pdo);
+
+			// update reply
+			$reply->message = "Player was updated OK";
+		} else if($method === "POST") {
+
+			//  make sure userId is available
+			if(empty($requestObject->userId) === true) {
+				throw(new \InvalidArgumentException ("No User ID.", 405));
+			}
+
+			// create new player and insert into the database
+			$player = new MlbScout\Player(null, $requestObject->userId, $requestObject->PlayerHealthStatus, null);
+			$player->insert($pdo);
+
+			// update reply
+			$reply->message = "Player was created OK";
+		}
+	} else if($method === "PUT" || $method === "POST") {
+
+		verifyXsrf();
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
+
+		//make sure player height is available
+		if(empty($requestObject->playerHeight) === true) {
+			throw(new \InvalidArgumentException ("No player Height.", 405));
+		}
+
+
+		//perform the actual put or post
+		if($method === "PUT") {
+
+			// retrieve the player to update
+			$player = MlbScout\Player::getPlayerByPlayerId($pdo, $id);
+			if($player === null) {
+				throw(new RuntimeException("player does not exist", 404));
+			}
+
+			// put the new player height into the player and update
+			$tweet->setPlayerHeight($requestObject->playerHeight);
+			$tweet->update($pdo);
+
+			// update reply
+			$reply->message = "Player was updated OK";
+		} else if($method === "POST") {
+
+			//  make sure userId is available
+			if(empty($requestObject->userId) === true) {
+				throw(new \InvalidArgumentException ("No User ID.", 405));
+			}
+
+			// create new player and insert into the database
+			$player = new MlbScout\Player(null, $requestObject->userId, $requestObject->PlayerHeight, null);
+			$player->insert($pdo);
+
+			// update reply
+			$reply->message = "Player was created OK";
+		}
+	} else if($method === "PUT" || $method === "POST") {
+
+		verifyXsrf();
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
+
+		//make sure player home town is available
+		if(empty($requestObject->playerHomeTown) === true) {
+			throw(new \InvalidArgumentException ("No player Home Town.", 405));
+		}
+
+
+		//perform the actual put or post
+		if($method === "PUT") {
+
+			// retrieve the player to update
+			$player = MlbScout\Player::getPlayerByPlayerId($pdo, $id);
+			if($player === null) {
+				throw(new RuntimeException("player does not exist", 404));
+			}
+
+			// put the new player home town into the player and update
+			$tweet->setPlayerHomeTown($requestObject->playerHomeTown);
+			$tweet->update($pdo);
+
+			// update reply
+			$reply->message = "Player was updated OK";
+		} else if($method === "POST") {
+
+			//  make sure userId is available
+			if(empty($requestObject->userId) === true) {
+				throw(new \InvalidArgumentException ("No User ID.", 405));
+			}
+
+			// create new player and insert into the database
+			$player = new MlbScout\Player(null, $requestObject->userId, $requestObject->PlayerHomeTown, null);
+			$player->insert($pdo);
+
+			// update reply
+			$reply->message = "Player was created OK";
+		}
+	} else if($method === "PUT" || $method === "POST") {
+
+		verifyXsrf();
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
+
+		//make sure player last name is available
+		if(empty($requestObject->playerLastName) === true) {
+			throw(new \InvalidArgumentException ("No player Last Name.", 405));
+		}
+
+
+		//perform the actual put or post
+		if($method === "PUT") {
+
+			// retrieve the player to update
+			$player = MlbScout\Player::getPlayerByPlayerId($pdo, $id);
+			if($player === null) {
+				throw(new RuntimeException("player does not exist", 404));
+			}
+
+			// put the new player last name into the player and update
+			$tweet->setPlayerLastName($requestObject->playerLastName);
+			$tweet->update($pdo);
+
+			// update reply
+			$reply->message = "Player was updated OK";
+		} else if($method === "POST") {
+
+			//  make sure userId is available
+			if(empty($requestObject->userId) === true) {
+				throw(new \InvalidArgumentException ("No User ID.", 405));
+			}
+
+			// create new player and insert into the database
+			$player = new MlbScout\Player(null, $requestObject->userId, $requestObject->PlayerLastName, null);
+			$player->insert($pdo);
+
+			// update reply
+			$reply->message = "Player was created OK";
+		}
+	} else if($method === "PUT" || $method === "POST") {
+
+		verifyXsrf();
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
+
+		//make sure player position is available
+		if(empty($requestObject->playerPosition) === true) {
+			throw(new \InvalidArgumentException ("No player position.", 405));
+		}
+
+
+		//perform the actual put or post
+		if($method === "PUT") {
+
+			// retrieve the player to update
+			$player = MlbScout\Player::getPlayerByPlayerId($pdo, $id);
+			if($player === null) {
+				throw(new RuntimeException("player does not exist", 404));
+			}
+
+			// put the new player position into the player and update
+			$tweet->setPlayerPosition($requestObject->playerPosition);
+			$tweet->update($pdo);
+
+			// update reply
+			$reply->message = "Player was updated OK";
+		} else if($method === "POST") {
+
+			//  make sure UserId is available
+			if(empty($requestObject->userId) === true) {
+				throw(new \InvalidArgumentException ("No User ID.", 405));
+			}
+
+			// create new player and insert into the database
+			$player = new MlbScout\Player(null, $requestObject->userId, $requestObject->PlayerPosition, null);
+			$player->insert($pdo);
+
+			// update reply
+			$reply->message = "Player was created OK";
+		}
+	} else if($method === "PUT" || $method === "POST") {
+
+		verifyXsrf();
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
+
+		//make sure player Throwing Hand is available
+		if(empty($requestObject->playerThrowingHand) === true) {
+			throw(new \InvalidArgumentException ("No player throwing hand.", 405));
+		}
+
+
+		//perform the actual put or post
+		if($method === "PUT") {
+
+			// retrieve the player to update
+			$player = MlbScout\Player::getPlayerByPlayerId($pdo, $id);
+			if($player === null) {
+				throw(new RuntimeException("player does not exist", 404));
+			}
+
+			// put the new player throwing hand into the player and update
+			$tweet->setPlayerThrowingHand($requestObject->playerThrowingHand);
+			$tweet->update($pdo);
+
+			// update reply
+			$reply->message = "Player was updated OK";
+		} else if($method === "POST") {
+
+			//  make sure UserId is available
+			if(empty($requestObject->userId) === true) {
+				throw(new \InvalidArgumentException ("No User ID.", 405));
+			}
+
+			// create new player and insert into the database
+			$player = new MlbScout\Player(null, $requestObject->userId, $requestObject->PlayerThrowingHand, null);
+			$player->insert($pdo);
+
+			// update reply
+			$reply->message = "Player was created OK";
+		}
+	} else if($method === "PUT" || $method === "POST") {
+
+		verifyXsrf();
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
+
+		//make sure player weight is available
+		if(empty($requestObject->playerWeight) === true) {
+			throw(new \InvalidArgumentException ("No player Weight.", 405));
+		}
+
+
+		//perform the actual put or post
+		if($method === "PUT") {
+
+			// retrieve the player to update
+			$player = MlbScout\Player::getPlayerByPlayerId($pdo, $id);
+			if($player === null) {
+				throw(new RuntimeException("player does not exist", 404));
+			}
+
+			// put the new player weight into the player and update
+			$tweet->setPlayerWeight($requestObject->playerWeight);
+			$tweet->update($pdo);
+
+			// update reply
+			$reply->message = "Player was updated OK";
+		} else if($method === "POST") {
+
+			//  make sure UserId is available
+			if(empty($requestObject->userId) === true) {
+				throw(new \InvalidArgumentException ("No User ID.", 405));
+			}
+
+			// create new player and insert into the database
+			$player = new MlbScout\Player(null, $requestObject->userId, $requestObject->PlayerWeight, null);
+			$player->insert($pdo);
+
+			// update reply
+			$reply->message = "Player was created OK";
+		}
 	} else if($method === "DELETE") {
 		verifyXsrf();
 
