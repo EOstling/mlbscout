d2409d31998fdb7218f2022cde65ac6ef7ad8ccc
Jared Padilla <jaredpadilla16@gmail.com>
2016-05-28 15:44:50 -0600

    Started User API

--- public_html/php/api/User/index.php
+++ public_html/php/api/User/index.php
@@ -1,135 +1,39 @@
 <?php
-
-require_once "autoload.php";
-require_once "/lib/xsrf.php";
-require_once("/etc/apache2/mlbscout-mysql/encrypted-config.php");
-
 use Edu\Cnm\MlbScout;
 
-
+require_once dirname(__DIR__, 2) . "/classes/autoload.php";
+require_once dirname(__DIR__, 2) . "/lib/xsrf.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 /**
- * api for the User class
+ * api for the User Class
  *
- * @author Jared Padilla <jaredpadilla16@gmail.com>
+ * @author Jared Padilla
  **/
-
-//verify the session, start if not active
+// verify the session, start if not active
 if(session_status() !== PHP_SESSION_ACTIVE) {
 	session_start();
 }
-
-//prepare an empty reply
+// prepare an empty reply
 $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
-
 try {
-	//grab the mySQL connection
-	$pdo = connectToEncryptedMySQL("/etc/apache2/mlbscout-mysql/user.ini");
-
-	//determine which HTTP method was used
+	// grab the mySQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/mlbscout.ini");
+	// determine which HTTP was used
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+	// sanitize input
+	$id = filter_input(INPUT_GET, "Id", FILTER_VALIDATE_INT);
+	$userId = filter_input(INPUT_GET, "userId", FILTER_VALIDATE_INT);
+	$userAccessLevelId = filter_input(INPUT_GET, "userAccessLevelId", FILTER_VALIDATE_INT);
+	$userActivationToken = filter_input(INPUT_GET, "userActivationToken", FILTER_SANITIZE_STRING);
+	$userEmail = filter_input(INPUT_GET, "userEmail", FILTER_SANITIZE_STRING);
+	$userFirstName = filter_input(INPUT_GET, "userFirstName", FILTER_SANITIZE_STRING);
+	$userHash = filter_input(INPUT_GET, "userHash", FILTER_SANITIZE_STRING);
+	$userLastName = filter_input(INPUT_GET, "userLastName", FILTER_SANITIZE_STRING);
+	$userPhoneNumber = filter_input(INPUT_GET, "userPhoneNumber", FILTER_VALIDATE_INT);
+	$userSalt = filter_input(INPUT_GET, "userSalt", FILTER )
 
-	//sanitize input
-	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
-
-	//make sure the id is valid for methods that require it
-	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)) {
-		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
-	}
-	
-	// handle GET request - if id is present, that user is returned, otherwise all users are returned
-	if($method === "GET") {
-		//set XSRF cookie
-		setXsrfCookie();
-
-		//get a specific user or all users and update reply
-		if(empty($id) === false) {
-			$user = MlbScout\User::getUserByUserId($pdo, $id);
-			if($user !== null) {
-				$reply->data = $user;
-			}
-		}  else {
-			$users = MlbScout\User::getAllUsers($pdo);
-			if($users !== null) {
-				$reply->data = $users;
-			}
-		}
-	} else if($method === "PUT" || $method === "POST") {
-
-		verifyXsrf();
-		$requestContent = file_get_contents("php://input");
-		$requestObject = json_decode($requestContent);
-
-		//make sure user content is available
-		if(empty($requestObject->tweetContent) === true) {
-			throw(new \InvalidArgumentException ("No content for User.", 405));
-		}
-
-
-		//perform the actual put or post
-		if($method === "PUT") {
-
-			// retrieve the tweet to update
-			$user = DataDesign\Tweet::getTweetByTweetId($pdo, $id);
-			if($user === null) {
-				throw(new RuntimeException("Tweet does not exist", 404));
-			}
-
-			// put the new tweet content into the tweet and update
-			$user->setTweetContent($requestObject->tweetContent);
-			$user->update($pdo);
-
-			// update reply
-			$reply->message = "Tweet updated OK";
-
-		} else if($method === "POST") {
-
-			//  make sure profileId is available
-			if(empty($requestObject->profileId) === true) {
-				throw(new \InvalidArgumentException ("No Pofile ID.", 405));
-			}
-
-			// create new tweet and insert into the database
-			$user = new DataDesign\Tweet(null, $requestObject->profileId, $requestObject->tweetContent, null);
-			$user->insert($pdo);
-
-			// update reply
-			$reply->message = "Tweet created OK";
-		}
-
-	} else if($method === "DELETE") {
-		verifyXsrf();
-
-		// retrieve the Tweet to be deleted
-		$user = DataDesign\Tweet::getTweetByTweetId($pdo, $id);
-		if($user === null) {
-			throw(new RuntimeException("Tweet does not exist", 404));
-		}
-
-		// delete tweet
-		$user->delete($pdo);
-
-		// update reply
-		$reply->message = "Tweet deleted OK";
-	} else {
-		throw (new InvalidArgumentException("Invalid HTTP method request"));
-	}
-
-	// update reply with exception information
-} catch(Exception $exception) {
-	$reply->status = $exception->getCode();
-	$reply->message = $exception->getMessage();
-	$reply->trace = $exception->getTraceAsString();
-} catch(TypeError $typeError) {
-	$reply->status = $typeError->getCode();
-	$reply->message = $typeError->getMessage();
 }
 
-header("Content-type: application/json");
-if($reply->data === null) {
-	unset($reply->data);
-}
 
-// encode and return reply to front end caller
-echo json_encode($reply);
