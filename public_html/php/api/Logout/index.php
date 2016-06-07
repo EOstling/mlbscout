681cd8381cc6e624d751a53ad54f0f6025a93312
Eliot Ostling <it.treugott@gmail.com>
2016-06-07 10:41:31 -0600

    adding signout api

--- public_html/php/api/Logout/index.php
+++ public_html/php/api/Logout/index.php
@@ -1,1 +1,19 @@
+
 <?php
+require_once dirname(__DIR__, 2) . "/classes/autoload.php";
+require_once dirname(__DIR__, 2) . "/lib/xsrf.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+use Edu\Cnm\MlbScout;
+/**
+ * api for signout
+ *
+ * @author Eliot Robert Ostling <it.treugott@gmail.com>
+ **/
+//POST is another possibility but were using "GET"
+if($method === "GET"){
+	if(session_status() !== PHP_SESSION_ACTIVE) {
+		session_start();
+	}
+	$_SESSION = []''
+}
+?>
