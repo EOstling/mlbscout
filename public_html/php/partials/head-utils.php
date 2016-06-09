215bdfaa92cef15a24d709cd957a11220089d7b8
Lucas Lauick <llaudick@cnm.edu>
2016-06-09 09:53:04 -0600

    added for contact us page

--- public_html/php/partials/head-utils.php
+++ public_html/php/partials/head-utils.php
@@ -17,17 +17,6 @@
 		<!-- jQuery -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 
-		<!-- jQuery Form, Additional Methods, Validate -->
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/additional-methods.min.js"></script>
-
-		<!-- Google reCAPTCHA -->
-		<script src='https://www.google.com/recaptcha/api.js'></script>
-
-		<!-- Your JavaScript Form Validator -->
-		<script src="<?php echo $PREFIX; ?>js/form-validate.js"></script>
-
 		<!--Angular JS Libraries-->
 		<?php $ANGULAR_VERSION = "1.5.6";?>
 		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/<?php echo $ANGULAR_VERSION;?>/angular.min.js"></script>
@@ -47,4 +36,16 @@
 		<script type="text/javascript" src="angular/controllers/search-controller.js"></script>
 		<script type="text/javascript" src="angular/controllers/results-controller.js"></script>
 		<script type="text/javascript" src="angular/controllers/contact-controller.js"></script>
+
+		<!-- jQuery Form, Additional Methods, Validate -->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/additional-methods.min.js"></script>
+
+		<!-- Google reCAPTCHA -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
+
+		<!-- Your JavaScript Form Validator -->
+		<script src="<?php echo $PREFIX; ?>js/form-validate.js"></script>
+
 	</head>
