90d842e9c22f1362e3726dd96d65fd593dcc403f
Lucas Lauick <llaudick@cnm.edu>
2016-06-09 09:21:11 -0600

    added links to head-utils

--- public_html/angular/templates/contactUs.php
+++ public_html/angular/templates/contactUs.php
@@ -9,9 +9,10 @@
 <div class="row">
 	<div class="col-xs-12">
 		<div class="well text-center">
-			<form class="form-horizontal well" action="email.php">
+			<!--Begin Contact Form-->
+			<form id="contact-form" action="php/mailer.php" method="post">
 				<div class="form-group">
-					<label for="name">Name</label>
+					<label for="name">Name <span class="text-danger">*</span></label>
 					<div class="input-group">
 						<div class="input-group-addon">
 							<i class="fa fa-user" aria-hidden="true"></i>
@@ -20,7 +21,7 @@
 					</div>
 				</div>
 				<div class="form-group">
-					<label for="email">Email address</label>
+					<label for="email">Email <span class="text-danger">*</span></label>
 					<div class="input-group">
 						<div class="input-group-addon">
 							<i class="fa fa-envelope" aria-hidden="true"></i>
@@ -38,17 +39,28 @@
 					</div>
 				</div>
 				<div class="form-group">
-					<label for="message">Message</label>
+					<label for="message">Message <span class="text-danger">*</span></label>
 					<div class="input-group">
 						<div class="input-group-addon">
 							<i class="fa fa-comment" aria-hidden="true"></i>
 						</div>
-						<textarea class="form-control" rows="5" id="message" name="message" placeholder="Message"></textarea>
+						<textarea class="form-control" rows="5" id="message" name="message" placeholder="Message (2000 characters max)"></textarea>
 					</div>
 				</div>
+
+				<!-- reCAPTCHA -->
+				<div class="g-recaptcha" data-sitekey="--YOUR RECAPTCHA SITE KEY--"></div>
+
 				<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
 				<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
 			</form>
+
+			<!--empty area for form error/success output-->
+			<div class="row">
+				<div class="col-xs-12">
+					<div id="output-area"></div>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
--- public_html/php/partials/head-utils.php
+++ public_html/php/partials/head-utils.php
@@ -14,6 +14,20 @@
 		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css" rel="stylesheet" />
 		<link rel="stylesheet" href="css/style.css" type="text/css" />
 
+		<!-- jQuery -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+
+		<!-- jQuery Form, Additional Methods, Validate -->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/additional-methods.min.js"></script>
+
+		<!-- Google reCAPTCHA -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
+
+		<!-- Your JavaScript Form Validator -->
+		<script src="<?php echo $PREFIX; ?>js/form-validate.js"></script>
+
 		<!--Angular JS Libraries-->
 		<?php $ANGULAR_VERSION = "1.5.6";?>
 		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/<?php echo $ANGULAR_VERSION;?>/angular.min.js"></script>
