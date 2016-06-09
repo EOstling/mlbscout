049f99b0122e311b2ef0884f86ee3fa4f4b6c4e0
Lucas Lauick <llaudick@cnm.edu>
2016-06-09 08:56:13 -0600

    added links to footer https://app.asana.com/0/117435667927176/118836678397140

--- public_html/php/partials/footer.php
+++ public_html/php/partials/footer.php
@@ -3,12 +3,11 @@
 		<div class="copyright text-center">
 			<div class="nav-center">
 				<ul class="nav nav-pills nav-justified">
-					<li role="presentation"><a href="#">Home</a></li>
-					<li role="presentation"><a href="#">About Us</a></li>
-					<li role="presentation"><a href="#">Players</a></li>
-					<li role="presentation"><a href="#">Search</a></li>
-					<li role="presentation"><a href="#">Profile</a></li>
-					<li role="presentation"><a href="#">Contact Us</a></li>
+					<li role="presentation"><a href="user-login">Home</a></li>
+					<li role="presentation"><a href="about-us">About Us</a></li>
+					<li role="presentation"><a href="search">Search</a></li>
+					<li role="presentation"><a href="player-profile">Profile</a></li>
+					<li role="presentation"><a href="contact-us">Contact Us</a></li>
 				</ul>
 			</div>
 			&copy; RealTimeScout Productions
