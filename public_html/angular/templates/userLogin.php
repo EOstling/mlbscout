fe7fd523ab4186761a6d0b660ebfb632f8b60a32
Lucas Lauick <llaudick@cnm.edu>
2016-06-09 08:53:23 -0600

    changed username to email https://app.asana.com/0/117435667927176/118836678397140

--- public_html/angular/templates/userLogin.php
+++ public_html/angular/templates/userLogin.php
@@ -11,12 +11,12 @@
 		<div class="well text-center">
 			<form class="form-horizontal well" action="email.php">
 				<div class="form-group">
-					<label for="Username">Username</label>
+					<label for="Email">Email</label>
 					<div class="input-group">
 						<div class="input-group-addon">
 							<i class="fa fa-user" aria-hidden="true"></i>
 						</div>
-						<input type="text" class="form-control" id="Username" name="Username" placeholder="Username">
+						<input type="text" class="form-control" id="Email" name="Email" placeholder="Email">
 					</div>
 				</div>
 				<div class="form-group">
