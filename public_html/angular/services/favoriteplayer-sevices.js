4e9f6b30588f85f4c9a1f66b126a8d0bdc1ae586
Jared Padilla <jaredpadilla16@gmail.com>
2016-06-10 09:58:58 -0600

    favoritePlayer service

---
+++ public_html/angular/services/favoriteplayer-sevices.js
@@ -0,0 +1,30 @@
+app.constant("FavoritePlayer_ENDPOINT", "api/favoriteplayer/");
+app.service("FavoritePlayerService", function($http, FAVORITEPLAYER_ENDPOINT) {
+	function getUrl() {
+		return(FavoritePlayer_ENDPOINT);
+	}
+
+	function getUrlForId(favoritePlayerId) {
+		return(getUrl() + favoritePlayerId);
+	}
+
+	this.all = function() {
+		return($http.get(getUrl()));
+	};
+
+	this.fetch = function(favoritePlayerId) {
+		return($http.get(getUrlForId(favoritePlayerId)));
+	};
+
+	this.create = function(favoritePlayer) {
+		return($http.post(getUrl(), favoritePlayer));
+	};
+
+	this.update = function(favoritePlayerId, favoritePlayer) {
+		return($http.put(getUrlForId(favoritePlayerId), favoritePlayer));
+	};
+
+	this.destroy = function(favoritePlayerId) {
+		return($http.delete(getUrlForId(favoritePlayerId)));
+	};
+});
