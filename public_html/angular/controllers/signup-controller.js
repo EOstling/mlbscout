f4d0f286eeb8a36e75122b307c0c9de874cdb364
Lucas Lauick <llaudick@cnm.edu>
2016-06-13 16:43:33 -0600

    edited signup controller

--- public_html/angular/controllers/signup-controller.js
+++ public_html/angular/controllers/signup-controller.js
@@ -8,14 +8,4 @@ app.controller('signupController', [function($scope) {
 				});
 		}
 	};
-	$scope.updateSignup = function(signup, validated) {
-		if(validated === true) {
-			SignupService.update(signup.userId, signup)
-				.then(function(result) {
-					$scope.displayStatus(result.data);
-					$scope.cancelEditing();
-					$scope.getsignups();
-				});
-		}
-	};
 }]);
