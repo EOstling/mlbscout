e6157473f493083ba33548fef07f25724717de82
Eliot Ostling <it.treugott@gmail.com>
2016-06-13 16:40:47 -0600

    adding login controller...controller

--- public_html/angular/controllers/user-controller.js
+++ public_html/angular/controllers/user-controller.js
@@ -1,3 +1,13 @@
 app.controller('userController', ["$scope", "$window",function($scope) {
 
+	$scope.getUser = function() {
+		UserService.all()
+			.then(function(result) {
+				if(result.data.status === 200) {
+					$scope.misquotes = result.data.data;
+				} else {
+					$scope.alerts[0] = {type: "danger", msg: result.data.message};
+				}
+			});
+	};
 }]);
