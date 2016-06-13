f9e24b469860d21186beaaf126f6a35cfb764381
Eliot Ostling <it.treugott@gmail.com>
2016-06-13 13:39:51 -0600

    finished activation token

--- public_html/php/api/ActivationToken/index.php
+++ public_html/php/api/ActivationToken/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once(dirname(__DIR__, 3) . "/vendor/autoload.php");
+require_once(dirname(__DIR__, 4) . "/vendor/autoload.php");
 require_once dirname(dirname(__DIR__)) . "/classes/autoload.php";
 require_once dirname(dirname(__DIR__)) . "/lib/xsrf.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
@@ -32,18 +32,28 @@ try {
 		$user->setUserActivationToken(null);
 		$user->update($pdo);
 		$reply->message = "Thank gulag! User is activated!";
+
+		$message = <<< EOF
+<h1>Welcome to Gulag!</h1>
+<p>Mr Lucas welcomes you to Gulag, for the tiny amount of time you survive.</p>
+EOF;
+
 		//Use code from Gulag.ru
 		$swiftMessage = Swift_Message::newInstance();
-		$swiftMessage->setFrom([$email => $name]);
-		$recipients = [$email => "Welcome to Gulag Betches"];
+		$swiftMessage->setFrom(["gulag@bootcamp-coders.cnm.edu" => "Mr Lucas, Gulag Guard"]);
+		$recipients = [$user->getUserEmail() => $user->getUserFirstName() . " " . $user->getUserLastName()];
 		$swiftMessage->setTo($recipients);
-		$swiftMessage->setSubject($subject);
+		$swiftMessage->setSubject("Welcome to Gulag Betches");
 		$swiftMessage->setBody($message, "text/html");
 		$swiftMessage->addPart(html_entity_decode($message), "text/plain");
 
-
-
-
+		$smtp = Swift_SmtpTransport::newInstance("localhost", 25);
+		$mailer = Swift_Mailer::newInstance($smtp);
+		$numSent = $mailer->send($swiftMessage, $failedRecipients);
+		if($numSent !== count($recipients)) {
+			// the $failedRecipients parameter passed in the send() method now contains contains an array of the Emails that failed
+			throw(new RuntimeException("unable to send email"));
+		}
 	} else {
 		throw(new \Exception("Invalid HTTP method"));
 	}
