e1ebe980f2e20fdb03bd8dd559c266ca808302ff
Eliot Ostling <it.treugott@gmail.com>
2016-06-13 17:54:06 -0600

    made changes to signup controller

--- public_html/angular/controllers/signup-controller.js
+++ public_html/angular/controllers/signup-controller.js
@@ -1,10 +1,17 @@
 app.controller('signupController', [function($scope) {
 
-	$scope.createSignup = function(Signup, validated) {
+	$scope.createSignup = function(signup, validated) {
 		if(validated === true) {
-			SignupService.create(signup)
+			userService.create(user)
 				.then(function(result) {
-					$scope.displayStatus(result.data);
+					if(result.data.status === 200) {
+						$scope.alerts[0] = {type: "success", msg: result.data.message};
+						$scope.sampleForm = {firstName: null, Lastname: null, phoneNumber: null, email: null, password: null};
+						$scope.signUp.$setPristine();
+						$scope.signUp.$setUntouched();
+					} else {
+						$scope.alerts[0] = {type: "danger", msg: result.data.message};
+					}
 				});
 		}
 	};
