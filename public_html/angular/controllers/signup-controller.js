f24d3743b0301be712eba8fa9605b7a006058171
Lucas Lauick <llaudick@cnm.edu>
2016-06-13 16:40:15 -0600

    added content to signup and fixed signup and userLogin

--- public_html/angular/controllers/signup-controller.js
+++ public_html/angular/controllers/signup-controller.js
@@ -1,3 +1,21 @@
 app.controller('signupController', [function($scope) {
 
+	$scope.createSignup = function(Signup, validated) {
+		if(validated === true) {
+			SignupService.create(signup)
+				.then(function(result) {
+					$scope.displayStatus(result.data);
+				});
+		}
+	};
+	$scope.updateSignup = function(signup, validated) {
+		if(validated === true) {
+			SignupService.update(signup.userId, signup)
+				.then(function(result) {
+					$scope.displayStatus(result.data);
+					$scope.cancelEditing();
+					$scope.getsignups();
+				});
+		}
+	};
 }]);
