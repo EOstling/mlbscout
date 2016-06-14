30ad80d3523764ce67ff63e868ffc8d79de97665
Lucas Lauick <llaudick@cnm.edu>
2016-06-14 14:50:07 -0600

    added content to aboutUs.php

--- public_html/angular/templates/aboutUs.php
+++ public_html/angular/templates/aboutUs.php
@@ -10,7 +10,7 @@
 	<div class="col-xs-12">
 		<div class="well text-center">
 			<div class="well text-center">
-				<h3>RealTimeScout</h3>
+				<p>RealTimeScout is making it easier for scouts to find players right at their fingertips. It allows the scouts to be able to view all the players they want all in one place. We noticed a problem when it came to scouting for big time scouts and college scouts, there was no central location where they can find all the data they need on the players they want to know about. RealTimeScout gives these scouts that option. Most big time scouts don't spend their time behind desks looking for players, they are traveling and they need that quick and easy way to find players with the limited amount of time they have. Instead of scouts having to go to each individual website of the players team and only finding so much data on the player, scouts can now get even more data and not have the hassle of loading hundreds of websites. Not only can you get hard data on the players you also get to see their next 5 games of the schedule and where and when they will be playing so that scouts can plan accordingly. RealTimeScout is a great tool for scouts both at the college level and professional level, and it is a great way for all these players to get their recognition.</p>
 			</div>
 		</div>
 	</div>
