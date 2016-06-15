08f226a48252715d8882c899217a314a9645533c
Lucas Lauick <llaudick@cnm.edu>
2016-06-14 20:05:12 -0600

    changed some stuff

--- public_html/angular/controllers/schedule-controller.js
+++ public_html/angular/controllers/schedule-controller.js
@@ -1,16 +1,16 @@
-app.controller('scheduleController', ["$routeParams", "$scope", "ScheduleService", function($routeParams, $scope, ScheduleService) {
-	$scope.schedule = null;
-
-	$scope.getSchedule = function() {
-		ScheduleService.fetch($routeParams.id)
-			.then(function(result){
-				if(result.data.status === 200) {
-					$scope.schedule = result.data.data;
-				}
-			});
-	};
-
-	if($scope.schedule === null) {
-		$scope.getSchedule();
-	}
-}]);
+//app.controller('scheduleController', ["$routeParams", "$scope", "ScheduleService", function($routeParams, $scope, ScheduleService) {
+//	$scope.schedule = null;
+//
+//	$scope.getSchedule = function() {
+//		ScheduleService.fetch($routeParams.id)
+//			.then(function(result){
+//				if(result.data.status === 200) {
+//					$scope.schedule = result.data.data;
+//				}
+//			});
+//	};
+//
+//	if($scope.schedule === null) {
+//		$scope.getSchedule();
+//	}
+//}]);
