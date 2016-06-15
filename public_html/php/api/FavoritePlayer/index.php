18ee10f505c7d3fbf4c7ad2e173a27f1e0484234
Eliot Ostling <it.treugott@gmail.com>
2016-06-15 09:20:28 -0600

    making sessions for the stuff with the stuff pt 4

--- public_html/php/api/FavoritePlayer/index.php
+++ public_html/php/api/FavoritePlayer/index.php
@@ -61,9 +61,10 @@ try {
 //				$reply->data = $favoritePlayer;
 //			}
 		} else {
-			$favoritePlayer = new MlbScout\FavoritePlayer($requestObject->favoritePlayerPlayerId, $_SESSION["user"]->getUserId());
 			// here you return all favorite players for the user in the $_SESSION
+			$favoritePlayer = new MlbScout\FavoritePlayer($requestObject->favoritePlayerPlayerId, $_SESSION["user"]->getUserId());
 		}
+
 	} else if($method === "POST") {
 
 		verifyXsrf();
