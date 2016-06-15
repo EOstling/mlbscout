025ceff485487838535d9d1b11bb2ab0b861f1c3
Lucas Lauick <llaudick@cnm.edu>
2016-06-14 18:50:24 -0600

    finding place for favorite button

--- public_html/angular/templates/playerProfile.php
+++ public_html/angular/templates/playerProfile.php
@@ -1,4 +1,5 @@
 <div class="row">
+	<button type="button" ng-click="favoritePlayer();">Favorite Prisoner</button>
 	<section class="col-md-9">
 		<div class="col-md-6">
 			<div class="well text-center">
@@ -25,11 +26,11 @@
 				<p>HomeTown: {{ player.playerHomeTown }}</p>
 				<p>Health Status: {{ player.playerHealthStatus }}</p>
 			</div>
-			<button type="button" ng-click="favoritePlayer();">Favorite Prisoner</button>
 		</div>
 		<div class="col-md-12">
 			<div class="well text-center">
 				<h4>Player Schedule</h4>
+				<h5>Location:</h5> <h5>Date:</h5>
 			</div>
 		</div>
 	</section>
